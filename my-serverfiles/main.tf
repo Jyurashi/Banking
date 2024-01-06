d34e9915b2457879fd2ff83807d29a95ac5e9b30
Jyurashi <144510416+Jyurashi@users.noreply.github.com>
2024-01-06 21:00:49 +0530

    Created main.tf

---
+++ my-serverfiles/main.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "test_server" {
+  ami = "ami-0a0f1259dd1c90938"
+  instance_type = "t2.micro"
+  key_name = "Jenkins"
+  vpc_security_group_ids = ["sg-0eb647279d57a624f"]
+  connection {
+     type		= "ssh"
+     user		= "ubuntu"
+     private_key	= file("./Jenkins.pem")
+     host 		= self.public_ip
+}
+
+provisioner "remote-exec" {
+    inline = ["echo 'wait to start instance' "]
+}
+tags = {
+  Name = "test-server"
+}
+provisioner "Local-exec" {
+    inline = "echo ${aws_instance.test-server.public_ip} > inventory "
+}
+provisioner "Local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/target/BP/target/my-serverfiles/finance-playbook.yml"
+}
+}
